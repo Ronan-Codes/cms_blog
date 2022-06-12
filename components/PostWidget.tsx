f363ca7aa4d8886f0132b382ae29267cac197fcb
Ronan-Codes <galv.ronan@gmail.com>
2022-06-12 01:56:20 -0400

    PostWidget mostly complete

--- components/PostWidget.tsx
+++ components/PostWidget.tsx
@@ -1,9 +1,64 @@
-import { NextPage } from 'next'
-import React from 'react'
+// @ts-nocheck
 
-const PostWidget: NextPage = () => {
+import { useState, useEffect } from 'react'
+import { NextPage } from 'next'
+import moment from 'moment'
+import Link from 'next/link'
+import { Category } from '../types'
+
+import { getRecentPosts, getSimilarPosts } from '../services'
+
+const PostWidget: NextPage<{categories?: Category[], slug?: string}> = ({ categories, slug}) => {
+  const [relatedPosts, setRelatedPosts] = useState([]);
+
+  useEffect(() => {
+    if(slug) {
+      getSimilarPosts(categories, slug)
+        .then((result) => {
+          setRelatedPosts(result)
+        })
+    } else {
+      getRecentPosts()
+        .then((result) => {
+          setRelatedPosts(result)
+        })
+    }
+  
+    // return () => {
+      
+    // }
+  }, [slug])
+  
+  console.log(relatedPosts)
+
   return (
-    <div>PostWidget</div>
+    <div className='bg-white shadow-lg rounded-lg p-8 mb-8'>
+      <h3 className='text-xl mb-8 font-semibold border-b pb-4'>
+        {slug? 'Related Posts' : "Recent Posts"}
+        {/* Home page does not have a slug in url so return Recent Posts */}
+      </h3>
+      {relatedPosts.map((post) => (
+        <div key={post.title} className='flex items-center w-full mb-4'>
+          <div className='w-16 flex-none'>
+            <img 
+              alt={post.title}
+              height="60px"
+              width="60px"
+              className='align-middle rounded-full'
+              src={post.featuredImage.url}
+            />
+          </div>
+          <div className='flex-grow ml-4'>
+            <p className='text-gray-500 font-xs'>
+              {moment(post.createdAt).format('MMM DD, YYYY')}
+            </p>
+            <Link href={`/post/${post.slug}`} className='text-md' key={post.title}>
+              {post.title}
+            </Link>
+          </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
--- services/index.tsx
+++ services/index.tsx
@@ -38,3 +38,51 @@ export const getPosts = async () => {
 
 	return result.postsConnection.edges;
 };
+
+export const getRecentPosts = async () => {
+    const query = gql `
+        query GetPostDetails() {
+            posts(
+                orderBy: createdAt_ASC
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+
+	return result.posts;
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+        query GetPostDetails($slug: String!, $categories: [String!]) {
+            posts(
+                where: {slug_not: $slug, AND: {categories_some: {slug_in: $categories}}}
+                last: 3
+            ) {
+                title
+                featuredImage {
+                    url
+                }
+                createdAt
+                slug
+            }
+        }
+    `
+
+    const result = await request(graphqlAPI, query);
+
+	return result.posts;
+}
+// _not: get posts, but not including the current post we're at, by checking slug_not
+// _some: But include other articles that include some of the categories that we want to get 
+// _in: included in slug
+// and we want to specify that we just want the last 3 articles  
